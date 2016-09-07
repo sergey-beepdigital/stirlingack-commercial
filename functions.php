6fa76e8244f3605f3aa9dfbf1ce81ed14df2f22b
Ralph Saunders <ralphsaunders@Ralphs-iMac.local>
2016-09-07 15:42:14 +0100

    Retab functions.php

--- functions.php
+++ functions.php
@@ -1,23 +1,23 @@
 <?php
 
 if ( ! class_exists( 'Timber' ) ) {
-	add_action( 'admin_notices', function() {
-			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
-		} );
-	return;
+    add_action( 'admin_notices', function() {
+        echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+    } );
+    return;
 }
 
 Timber::$dirname = array('templates', 'views');
 
 class StarterSite extends TimberSite {
 
-	function __construct() {
-		add_theme_support( 'post-formats' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'menus' );
+    function __construct() {
+        add_theme_support( 'post-formats' );
+        add_theme_support( 'post-thumbnails' );
+        add_theme_support( 'menus' );
 
-		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+        add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+        add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
         add_filter( 'upload_mimes', array($this, 'svg_mime_types' ));
 
         // Disable WP-REST API for Security
@@ -31,7 +31,7 @@ class StarterSite extends TimberSite {
         remove_action('rest_api_init', 'wp_oembed_register_route');
         remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
 
-		add_action( 'init', array( $this, 'register_post_types' ) );
+        add_action( 'init', array( $this, 'register_post_types' ) );
         add_action( 'init', array( $this, 'register_taxonomies' ) );
         add_action( 'init', array( $this, 'disable_wp_emojicons' ) );
         add_action('inline_file', array($this, 'inline_file'));
@@ -42,29 +42,29 @@ class StarterSite extends TimberSite {
             acf_add_options_page('Theme Options');
         }
 
-		parent::__construct();
-	}
+        parent::__construct();
+    }
 
-	function register_post_types() {
-		//this is where you can register custom post types
-	}
+    function register_post_types() {
+        //this is where you can register custom post types
+    }
 
-	function register_taxonomies() {
-		//this is where you can register custom taxonomies
-	}
+    function register_taxonomies() {
+        //this is where you can register custom taxonomies
+    }
 
-	function add_to_context( $context ) {
-		$context['menu'] = new TimberMenu('Global Header Navigation');
+    function add_to_context( $context ) {
+        $context['menu'] = new TimberMenu('Global Header Navigation');
         $context['site'] = $this;
         $context['options'] = get_fields('option');
         $context['page_stats'] = TimberHelper::start_timer();
-		return $context;
-	}
+        return $context;
+    }
 
-	function add_to_twig( $twig ) {
-		/* this is where you can add your own fuctions to twig */
-		$twig->addExtension( new Twig_Extension_StringLoader() );
-		return $twig;
+    function add_to_twig( $twig ) {
+        /* this is where you can add your own fuctions to twig */
+        $twig->addExtension( new Twig_Extension_StringLoader() );
+        return $twig;
     }
 
     function assets( $twig ) {
