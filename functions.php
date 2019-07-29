9d8d390bb2ebcdfdf99f5971b53044540d49b10e
Michael O'Mara <>
2019-07-29 16:46:18 +0100

    Removing un-needed css files and creating css reset

--- functions.php
+++ functions.php
@@ -392,6 +392,35 @@ function remove_editor_menu() {
 add_action('_admin_menu', 'remove_editor_menu', 1);
 
 /*
+*   Remove Gutenburg CSS
+*/
+function remove_block_css(){
+wp_dequeue_style( 'wp-block-library' );
+}
+add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
+
+/**
+ * Disable Yoast's Hidden love letter about using the WordPress SEO plugin.
+ */
+add_action( 'template_redirect', function () {
+ 
+    if ( ! class_exists( 'WPSEO_Frontend' ) ) {
+        return;
+    }
+ 
+    $instance = WPSEO_Frontend::get_instance();
+ 
+    // make sure, future version of the plugin does not break our site.
+    if ( ! method_exists( $instance, 'debug_mark') ) {
+        return ;
+    }
+ 
+    // ok, let us remove the love letter.
+     remove_action( 'wpseo_head', array( $instance, 'debug_mark' ), 2 );
+}, 9999 );
+
+
+/*
 *   Remove the detail from the wordpress errors
 */
 function no_wordpress_errors() {
