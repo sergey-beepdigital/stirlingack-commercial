b118fbd37c5a9cf0f580a77ac02a00e467aaf45d
Nathan <nathan@thisiscrowd.com>
2020-09-18 17:50:55 +0100

    Add social links shortcode, removed options page for Social Profiles as it can be done in Yoast

--- functions.php
+++ functions.php
@@ -127,7 +127,6 @@ class StarterSite extends TimberSite {
         if( function_exists('acf_add_options_page') ) {
             acf_add_options_sub_page('Theme');
             acf_add_options_sub_page('Analytics/Tracking');
-            acf_add_options_sub_page('Social Profiles');
 
             if (current_user_can('administrator') || get_field('show_debug_menu', 'option')) {
               acf_add_options_sub_page('Debug Options');
@@ -485,3 +484,67 @@ function noindex_author() {
     }
 }
 add_action('wp_head', 'noindex_author');
+
+function reset_social_link_icons() {
+    return array(
+        'facebook_site'  => array(
+            'icon' => '<i class="fab fa-facebook-f"></i>'
+        ),
+        'twitter_site'   => array(
+            'prepend' => 'https://twitter.com/',
+            'icon'    => '<i class="fab fa-twitter"></i>'
+        ),
+        'instagram_url' => array(
+            'icon' => '<i class="fab fa-instagram"></i>'
+        ),
+        'linkedin_url'  => array(
+            'icon' => '<i class="fab fa-linkedin-in"></i>'
+        )
+    );
+}
+add_filter('crowd_social_link_options','reset_social_link_icons');
+
+function social_links_shortcode($atts) {
+    $atts = shortcode_atts(array(
+        'list'      => 1, // true for a <ul>, false for <div>
+        'raw'       => 0, // true for text only, no HTML
+        'css_class' => '', // class to add to the wrapper
+        'delim'     => ' ' // entity between items
+    ), $atts);
+
+    $seo_data = get_option('wpseo_social');
+    $options = apply_filters('crowd_social_link_options', array());
+    $output = array();
+    $wrapp_tag = 'div';
+
+    if (sizeof($options) > 0) {
+        if (!$atts['raw']) {
+            if ($atts['list']) {
+                $wrapp_tag = 'ul';
+            }
+
+            $output[] = '<' . $wrapp_tag . ' class="' . $atts['css_class'] . '">';
+        }
+
+        foreach ($seo_data as $seo_network => $url) {
+            $network_settings = $options[$seo_network];
+
+            if(!empty($url) && !empty($network_settings)) {
+                if (!empty($network_settings['prepend']))
+                    $url = $network_settings['prepend'] . $url;
+                if ($url && !empty($network_settings['icon']) && !$atts['raw']) {
+                    if ($atts['list']) $output[] = '<li>';
+                    $output[] = '<a target="_blank" href="' . esc_url_raw($url) . '">' . $network_settings['icon'] . '</a>';
+                    if ($atts['list']) $output[] = '</li>';
+                } else {
+                    $output[] = esc_url_raw($url);
+                }
+            }
+        }
+        if (!$atts['raw'])
+            $output[] = '</' . $wrapp_tag . '>';
+    }
+
+    if (!empty($output)) return join($atts['delim'], $output);
+}
+add_shortcode('social_links', 'social_links_shortcode');
